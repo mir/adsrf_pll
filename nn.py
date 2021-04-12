89b22e6e4cdfb8472aadddb870b1ba87c084caa4
Marat Yuldashev <marat.spb@protonmail.com>
2021-04-12 18:48:51 +0300

    Add accuracy checking

--- nn.py
+++ nn.py
@@ -12,9 +12,9 @@ import random
 os.environ['KMP_DUPLICATE_LIB_OK'] = 'True'
 
 input_size = 2
-output_size = 64
+output_size = 16
 learning_rate = 0.01
-epochs = 32*32*32
+epochs = 32*32*2
 
 hidden_dim = 32
 theta_e_dim = 8
@@ -24,6 +24,20 @@ omega_e_max = 10.
 omega_e_range = torch.arange(-omega_e_max, omega_e_max, 2 * omega_e_max / omega_e_dim)
 total_dim = theta_e_dim*omega_e_dim
 
+test_points_number = 16
+
+def generate_test_points(test_points_number):
+    test_points = torch.rand(test_points_number, 2)
+    omega_s = torch.zeros(test_points_number, 1) + 2*omega_e_max
+    theta_s = torch.zeros(test_points_number, 1) + 2 * np.pi
+    scale = torch.cat((theta_s,omega_s), 1)
+    shift_omega = torch.zeros(test_points_number, 1) -omega_e_max
+    shift = torch.cat((torch.zeros(test_points_number, 1), shift_omega), 1)
+    test_points = test_points * scale + shift
+    return test_points
+
+test_points = generate_test_points(test_points_number)
+
 Ts = 10e-4
 a = 0.27198
 b = 0.5055
@@ -31,7 +45,6 @@ Omega = 0.144
 
 inputs = torch.zeros(total_dim, 2)
 labels = torch.zeros(total_dim, output_size)
-initial_condition = torch.zeros(total_dim, output_size)
 for i_theta, theta_e0 in enumerate(theta_e_range):
     for i_omega, omega_e in enumerate(omega_e_range):
         total_i = i_theta*theta_e_dim + i_omega
@@ -40,7 +53,6 @@ for i_theta, theta_e0 in enumerate(theta_e_range):
         x_k, theta_e_k = a_pll.steps(output_size)
         labels[total_i] = theta_e_k
         inputs[total_i] = torch.tensor([theta_e0, omega_e])
-        initial_condition[total_i] += theta_e0
 
 # Neural network model
 class NeuralPLLModel(nn.Module):
@@ -60,7 +72,6 @@ class NeuralPLLModel(nn.Module):
 
     def training_step(self, batch, labels):
         out = self(batch)
-        # out = out + initial_condition
         loss_fn = nn.MSELoss()
         return loss_fn(out, labels)
 
@@ -68,8 +79,20 @@ class NeuralPLLModel(nn.Module):
         input = torch.tensor([theta_e0, omega_e]).view(1, input_size)
         out = self(input)
         out = out[0].detach()
-        # out = out + theta_e0
         return out
+
+    def max_error(self, test_points):
+        acc_fn = nn.MSELoss()
+        max_error = 0
+        for step in range(test_points_number):
+            omega_e = test_points[step, 1]
+            theta_e0 = test_points[step, 0]
+            test_pll = pll.PLL(Ts, a, b, omega_e, Omega,
+                               0., theta_e0)
+            x_k, theta_e_k = test_pll.steps(output_size)
+            nn_theta_e_k = self.eval(theta_e0, omega_e)
+            max_error = max(max_error, acc_fn(theta_e_k, nn_theta_e_k))
+        return max_error
 
 def fit(epochs,
         learning_rate,
@@ -77,18 +100,21 @@ def fit(epochs,
         opt_func=torch.optim.Adam):
     """Train the model using gradient descent"""
     losses = []
+    max_errors = []
     optimizer = opt_func(model.parameters(), learning_rate)
     for it in range(epochs):
         hp.print_progress(epochs, it)
 
         loss = model.training_step(inputs, labels)
+        max_error = model.max_error(test_points)
         loss.backward()
         optimizer.step()
         optimizer.zero_grad()
         # log data
         losses.append(loss.item())
+        max_errors.append(max_error)
 
-    return losses
+    return losses, max_errors
 
 # generate data and data loaders
 
@@ -97,13 +123,8 @@ model = NeuralPLLModel()
 fig, (ax1, ax2) = plt.subplots(2, 2)
 fig.suptitle('SRF-PLL simulation NN')
 
-# Plot reference signal
-ax1[0].set_title('Theta_e_k (thetha_e0 = 0)')
-ax1[0].set_xlabel('k')
-ax1[0].plot(labels[0])
-
 print('Train NN')
-losses = fit(epochs, learning_rate, model)
+losses, max_errors = fit(epochs, learning_rate, model)
 
 def plot_random_nn_vs_real(axe):
     omega_e = random.random() * 2 * omega_e_max - omega_e_max
@@ -116,13 +137,19 @@ def plot_random_nn_vs_real(axe):
     nn_theta_e_k = model.eval(theta_e0, omega_e)
     axe.plot(nn_theta_e_k, color='g')
 
+plot_random_nn_vs_real(ax1[0])
 plot_random_nn_vs_real(ax1[1])
-plot_random_nn_vs_real(ax2[1])
 
 ax2[0].set_title('Training losses')
 ax2[0].set_ylabel('Loss')
 ax2[0].set_yscale('log')
 ax2[0].set_xlabel('epoch')
 ax2[0].plot(losses)
+
+ax2[1].set_title('Max errors')
+ax2[1].set_ylabel('max error')
+ax2[1].set_yscale('log')
+ax2[1].set_xlabel('epoch')
+ax2[1].plot(max_errors)
 
 plt.show()
